88efd37352670420b6850143ae38e9c9b92cc61e
Dmitriy Gritsenko <evil1@inbox.ru>
2025-04-02 21:43:06 +0500

    TicketRebookAndRepricePNR method

--- src/Amadeus/Client.php
+++ src/Amadeus/Client.php
@@ -2058,7 +2058,10 @@ class Client extends Base
         $messageOptions = $this->makeMessageOptions($messageOptions, $endSession);
 
         $this->lastMessage = $messageName;
-
+        \Yii::error(print_r($this->requestCreator->createRequest(
+            $messageName,
+            $options
+        ), true));
         $sendResult = $this->sessionHandler->sendMessage(
             $messageName,
             $this->requestCreator->createRequest(
