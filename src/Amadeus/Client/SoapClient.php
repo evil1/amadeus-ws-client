69bf5a3e3f82964436bb76103cf29e5ede9e8a55
Dmitriy Gritsenko <evil1@inbox.ru>
2026-01-01 09:24:57 +0500

    Fix __doRequest signature

--- src/Amadeus/Client/SoapClient.php
+++ src/Amadeus/Client/SoapClient.php
@@ -67,12 +67,13 @@ class SoapClient extends \SoapClient implements Log\LoggerAwareInterface
      * @param string $action The SOAP action.
      * @param int $version The SOAP version.
      * @param bool $oneWay
+     * @param string|null $uriParserClass
      * @return string|null The XML SOAP response.
      * @throws Exception When PHP XSL extension is not enabled or WSDL file isn't readable.
      * @uses parent::__doRequest
      */
     #[\ReturnTypeWillChange]
-    public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false): ?string
+    public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false, ?string $uriParserClass = null): ?string
     {
         if (!extension_loaded('xsl')) {
             throw new Exception('PHP XSL extension is not enabled.');
